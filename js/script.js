bd023507035852f781081390fb83d6436b9df25d
Nico Caro <nicolascaro926@gmail.com>
2021-10-04 14:05:32 +0200

    v1

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@
 // Fonction appelée lors du click du bouton
 function start() {
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  let city = document.getElementById('city-input').value;
+  const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
     .fetchTodayForecast()
-    .then(function(response) {
+    .then(function (response) {
       // Récupère la donnée d'une API
       const data = response.data;
 
-      // On récupère l'information principal
+      // On récupère l'information principale
       const main = data.weather[0].main;
       const description = data.weather[0].description;
       const temp = data.main.temp;
@@ -22,9 +23,9 @@ function start() {
       document.getElementById('today-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
+
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // Affiche une erreur
       console.error(error);
     });
